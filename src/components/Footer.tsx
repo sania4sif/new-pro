b011b8c6eb41c402fc076403d9bdb67b4e658583
sania-asif <sania.asif.mughal@gmail.com>
2024-10-20 10:24:58 -0700

    newproject

---
+++ src/components/Footer.tsx
@@ -0,0 +1,41 @@
+import { footer } from 'framer-motion/client'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-black text-gray-400 py-12 bottom-0 left-0 w-full'>
+    <div className='max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8'>
+    <div>
+        <h3 className='text-lg font-bold text-white mb-4 '>About us</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+    </div>
+    <div>
+        <h3 className='text-lg font-bold text-white mb-4'>
+            link
+        </h3>
+        <ul>
+            <li><a href="/"className='hover:text-slate-300'>home</a></li>
+            <li><a href="/courses"className='hover:text-slate-300'>courses</a></li>
+            <li><a href="/about"className='hover:text-slate-300'>about</a></li>
+            <li><a href="/contact"className='hover:text-slate-300'>contact</a></li>
+        </ul>
+    </div>
+    <div>
+        <h3 className='text-lg font-bold text-white mb-4'>follow us</h3>
+        <li><a href="/"className='hover:text-slate-300'>home</a></li>
+            <li><a href="/courses"className='hover:text-slate-300'>courses</a></li>
+            <li><a href="/about"className='hover:text-slate-300'>about</a></li>
+            <li><a href="/contact"className='hover:text-slate-300'>contact</a></li>
+    </div>
+    <div>
+        <h3 className='text-lg font-bold text-white mb-4'>contact us</h3>
+        <p>email <a href="/.mail@gmail.com">sania.asif.mughal@gmail.com</a></p>
+        <p>phone: <span className='hover:text-white'>03235776349</span></p>
+    </div>
+    </div>
+     <p className='flex justify-center p-2'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
+     </footer>
+  )
+}
+
+export default Footer
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,28 @@
+import { PluginAPI } from "tailwindcss/types/config";
 import type { Config } from "tailwindcss";
+function addVariableForColors({ addBase, theme }: PluginAPI){
+  const allColors = theme("colors",{}) || {};
 
+    const newVars: Record<string, string> = {};
+
+    Object.entries(allColors).forEach(([key, value]) => {
+      if (typeof value === "string") {
+        newVars[`--${key}`] = value;
+      } 
+
+      else if (typeof value === 'object' && value !== null) {
+        Object.entries(value).forEach(([subKey, subValue]) => {
+
+          if (typeof subValue === 'string') {
+            newVars[`--${key}-${subKey}`] = subValue;
+          }
+        });
+      }
+    });
+    addBase({
+      ':root': newVars,
+    });
+}
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,8 +35,23 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariableForColors],
 };
 export default config;
